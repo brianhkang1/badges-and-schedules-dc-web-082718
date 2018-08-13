741a9e86304c1e7a7a62e8b36dcd34707556207b
Brian Kang <brianhkang1@gmail.com>
2018-08-13 21:11:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(name)
-  people.each do |name|
-    puts "Hello, my name is #{name}."
+  name.each do |person|
+    puts "Hello, my name is #{person}."
   end
 end
 
