4eabce5b088d45914b564186e99ba984cd184f8b
Brian Kang <brianhkang1@gmail.com>
2018-08-13 21:03:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_badge_creator(people)
+  people.each do |name|
+    puts "Hello, my name is #{name}."
+  end 
+end
+
+def assign_rooms(people)
+  room = 1 
+  people.each do |name|
+    puts "Hello, #{name}! You'll be assigned to room #{room}!"
+    room += 1 
+  end
+end
